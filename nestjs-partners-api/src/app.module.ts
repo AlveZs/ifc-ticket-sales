85613aabfac8532e9638fd61c057eb2d01e1c73a
João Alves <alvezsjv@gmail.com>
2024-07-08 17:51:37 -0300

    feat(api/event): add CRUD for event

--- nestjs-partners-api/src/app.module.ts
+++ nestjs-partners-api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventController } from './event/event.controller';
+import { EventsModule } from './events/events.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [EventsModule, PrismaModule],
+  controllers: [AppController, EventController],
   providers: [AppService],
 })
 export class AppModule {}
