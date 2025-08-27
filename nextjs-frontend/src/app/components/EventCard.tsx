aa110ca8070d68dd530bf613516a626ca84aed88
João Alves <alvezsjv@gmail.com>
2025-08-27 19:19:59 -0300

    fix(next/eventCard): fix width typo and card height

--- nextjs-frontend/src/app/components/EventCard.tsx
+++ nextjs-frontend/src/app/components/EventCard.tsx
@@ -10,7 +10,7 @@ export type EventCardProps = {
 export function EventCard(props: PropsWithChildren<EventCardProps>) {
   return (
     <Link href={`/event/${props.event.id}/spots-layout`}>
-      <div className="flex w-[277p] flex-col rounded-2xl bg-secondary">
+      <div className="flex w-[277px] h-full flex-col rounded-2xl bg-secondary">
         <EventImage src={props.event.image_url} alt={props.event.name} />
         <div className="flex flex-col gap-y-2 px-4 py-6" >
           <p className="text-sm uppercase text-subtitle">
